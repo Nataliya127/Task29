3f448ab02be392502d9fe1a6e4552a7678700883
Nataliya <Natalia.konyaeva@yandex.ru>
2022-09-22 19:36:58 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 29: Пользователь вводит 8 чисел. Поместите их в массив и распечатайте.
+
+
+int [] number = new int[8];
+Console.Write("{");
+
+for (int i = 0; i < number.Length; i++)
+ {
+    number [i] = new Random().Next(0, 11);
+    Console.Write(" " + Method (i) + " ");
+ }
+Console.Write("}");
+
+int Method (int a)
+{
+    return number[a];
+}
+
+
+ 
